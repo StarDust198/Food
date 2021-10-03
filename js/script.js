bc130d52f63a175767388bee6c03419bca70bd31
StarDust85z <roadtomars2030@gmail.com>
2021-10-03 04:35:46 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
+
+    // tabs
     
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    const hideTabContent = function () {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach (item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    };
+
+    const showTabContent = function (i = 0) {
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+        tabs[i].classList.add('tabheader__item_active');
+    };
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (item === e.target) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
 });
