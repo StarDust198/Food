15a36cce7a7aac1d804a80b0df0b2526c0fcb011
StarDust85z <roadtomars2030@gmail.com>
2021-10-03 18:28:49 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -79,4 +79,35 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     setClock('.timer', deadline);
+
+    // modal
+
+    const modalBtns = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalClose = modal.querySelector('[data-close]'),
+          openModal = function() {
+            modal.classList.remove('hide');
+            modal.classList.add('show');
+            document.body.style.overflow = 'hidden';
+          },
+          closeModal = function() {
+            modal.classList.remove('show');
+            modal.classList.add('hide');
+            document.body.style.overflow = '';
+          };
+
+    modalBtns.forEach(btn => btn.addEventListener('click', openModal));
+
+    modal.addEventListener('click', (e) => {
+        if (e.target && (e.target.classList.contains('modal') ||
+            e.target == modalClose)) {
+                closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
