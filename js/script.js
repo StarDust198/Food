04de03289a402bd68e514d7fdf3df9d1593e3e8b
StarDust85z <roadtomars2030@gmail.com>
2021-10-04 22:29:12 +0300

    cards update

--- js/script.js
+++ js/script.js
@@ -89,20 +89,20 @@ window.addEventListener('DOMContentLoaded', () => {
             modal.classList.remove('hide');
             modal.classList.add('show');
             document.body.style.overflow = 'hidden';
-            clearInterval(modalTimerId);
+            // clearInterval(modalTimerId);
           },
           closeModal = function() {
             modal.classList.remove('show');
             modal.classList.add('hide');
             document.body.style.overflow = '';
           },
-          modalTimerId = setTimeout(openModal, 60000),
+        //   modalTimerId = setTimeout(openModal, 60000),
           showModalByScroll = function() {
             if (window.pageYOffset + document.documentElement.clientHeight >= 
                 document.documentElement.scrollHeight) {
                     openModal();
                     window.removeEventListener('scroll', showModalByScroll);
-                    clearInterval(modalTimerId);
+                    // clearInterval(modalTimerId);
             }
           };
 
@@ -124,45 +124,58 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // cards
 
-    const menuField = document.querySelector('.menu__field .container');
-    menuField.innerHTML = '';
+    document.querySelector('.menu__field .container').innerHTML = '';
 
     const cardsDB = [
         {
             img: 'img/tabs/vegy.jpg',
             alt: 'vegy',
             header: 'Меню "Фитнес"',
-            text: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            text: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих ' +
+            'овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый ' + 
+            'продукт с оптимальной ценой и высоким качеством!',
             price: 229
         },
         {
             img: 'img/tabs/elite.jpg',
             alt: 'elite',
             header: 'Меню “Премиум”',
-            text: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            text: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и ' + 
+            'качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное ' +
+            'меню без похода в ресторан!',
             price: 550
         },
         {
             img: 'img/tabs/post.jpg',
             alt: 'post',
             header: 'Меню "Постное"',
-            text: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            text: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие ' +
+            'продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, ' +
+            'правильное количество белков за счет тофу и импортных вегетарианских стейков.',
             price: 430
         }
     ];
 
-    class Card {
-        constructor(img, alt, header, text, price) {
+    class MenuCard {
+        constructor(img, alt, header, text, price, parentSelector, ...classes) {
             this.img = img;
             this.alt = alt;
             this.header = header;
             this.text = text;
             this.price = price;
+            this.transfer = 27;
+            // this.changeToUAH();
+            this.parent = document.querySelector(parentSelector);
+            this.classes = classes.length > 0 ? classes : ['menu__item'];
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
         }
 
         createCard() {
             const card = document.createElement('div');
-            card.classList.add('menu__item');
+            card.classList.add(...this.classes);
 
             card.innerHTML = `
                 <img src="${this.img}" alt="${this.alt}">
@@ -175,13 +188,12 @@ window.addEventListener('DOMContentLoaded', () => {
                 </div>
             `;
 
-            return card;
+            this.parent.append(card);
         }
     }
 
     for (let i in cardsDB) {
         const {img, alt, header, text, price} = cardsDB[i];
-        const newCard = new Card(img, alt, header, text, price);
-        menuField.append(newCard.createCard());
+        new MenuCard(img, alt, header, text, price, '.menu__field .container').createCard();
     }
 });
