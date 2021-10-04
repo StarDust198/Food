d8f3b595e63c7639d085970517a2ec89df53684a
StarDust85z <roadtomars2030@gmail.com>
2021-10-04 04:10:54 +0300

    cards class update, my solution

--- js/script.js
+++ js/script.js
@@ -98,7 +98,6 @@ window.addEventListener('DOMContentLoaded', () => {
           },
           modalTimerId = setTimeout(openModal, 60000),
           showModalByScroll = function() {
-            console.log(window.scrollHeight);
             if (window.pageYOffset + document.documentElement.clientHeight >= 
                 document.documentElement.scrollHeight) {
                     openModal();
@@ -122,4 +121,67 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     window.addEventListener('scroll', showModalByScroll);
+
+    // cards
+
+    const menuField = document.querySelector('.menu__field .container');
+    menuField.innerHTML = '';
+
+    const cardsDB = [
+        {
+            img: 'img/tabs/vegy.jpg',
+            alt: 'vegy',
+            header: 'Меню "Фитнес"',
+            text: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            price: 229
+        },
+        {
+            img: 'img/tabs/elite.jpg',
+            alt: 'elite',
+            header: 'Меню “Премиум”',
+            text: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            price: 550
+        },
+        {
+            img: 'img/tabs/post.jpg',
+            alt: 'post',
+            header: 'Меню "Постное"',
+            text: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            price: 430
+        }
+    ];
+
+    class Card {
+        constructor(img, alt, header, text, price) {
+            this.img = img;
+            this.alt = alt;
+            this.header = header;
+            this.text = text;
+            this.price = price;
+        }
+
+        createCard() {
+            const card = document.createElement('div');
+            card.classList.add('menu__item');
+
+            card.innerHTML = `
+                <img src="${this.img}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.header}</h3>
+                <div class="menu__item-descr">${this.text}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+
+            return card;
+        }
+    }
+
+    for (let i in cardsDB) {
+        const {img, alt, header, text, price} = cardsDB[i];
+        const newCard = new Card(img, alt, header, text, price);
+        menuField.append(newCard.createCard());
+    }
 });
